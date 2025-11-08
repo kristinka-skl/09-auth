8836ec9797d26808bbe5728f95242a942b492624
kristinka-skl <kristinka.skl@gmail.com>
2025-11-08 18:24:57 +0100

    change baseUrl

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextServer = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
   withCredentials: true,
 });
