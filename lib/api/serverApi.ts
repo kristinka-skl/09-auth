63e064b7dcfc67473be6b6d831637a628a7b19f6
kristinka-skl <kristinka.skl@gmail.com>
2025-11-08 18:48:05 +0100

    upd serverApi

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,13 +1,7 @@
-//  lib/api/serverApi.ts — для функцій, які викликаються у серверних компонентах (до params потрібно додавати cookeis у headers):
-
-// fetchNotes
-// fetchNoteById
-// getMe
-// checkSession.
-
 import { cookies } from "next/headers";
 import { nextServer } from "./api";
 import { User } from "@/types/user";
+import { Note } from "@/types/note";
 
 export const checkServerSession = async () => {
   const cookieStore = await cookies();
@@ -21,6 +15,40 @@ export const checkServerSession = async () => {
 export const getServerMe = async (): Promise<User> => {
   const cookieStore = await cookies();
   const { data } = await nextServer.get("/users/me", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
+};
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+const PER_PAGE = 12;
+
+export async function fetchNotes(
+  query: string,
+  page: number,
+  category?: string
+): Promise<FetchNotesResponse> {
+  const cookieStore = await cookies();
+  const { data } = await nextServer.get<FetchNotesResponse>("/notes", {
+    params: {
+      search: query,
+      page: page,
+      perPage: PER_PAGE,
+      tag: category,
+    },
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
+}
+export const fetchNoteById = async (id: string) => {
+  const cookieStore = await cookies();
+  const { data } = await nextServer.get<Note>(`/notes/${id}`, {
     headers: {
       Cookie: cookieStore.toString(),
     },
